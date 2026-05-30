eb530d40443685b1892ee1e3db91c14594653d38
Azuzula <azuzula.cz@gmail.com>
2026-05-30 10:14:52 +0200

    Add TypeScript definitions for PearBitten component

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface PearBittenProps extends React.SVGProps<SVGSVGElement> {
+  size?: number | string;
+  color?: string;
+}
+
+export declare const PearBitten: React.ComponentType<PearBittenProps>;
